c058b2cc4e70f90fb770b5e2c5dcc05df894eda2
Oleksandr Dziuba <58835413+olexdziuba@users.noreply.github.com>
2020-10-04 14:33:15 -0400

    Update systeme_info.sh

--- systeme_info.sh
+++ systeme_info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 #   Check:
-#   - Memory usage. disk space and disk partitioning information
+#   - Memory usage, disk space and disk partitioning information
 #   - CPU load
 #   - Number of TCP connections 
 #   - Kernel version
