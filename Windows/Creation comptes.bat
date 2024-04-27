0e65fdeb15d4df2263b9ddbd09b40b9ead8fe7a2
Oleksandr Dziuba <alex.dzuba@gmail.com>
2024-04-27 19:06:55 -0400

    new file:   Windows/Creation comptes.bat

---
+++ Windows/Creation comptes.bat
@@ -0,0 +1,8 @@
+Net user Jean 123456 /add /active:Yes /comment:"Compte Admin" /expires:Never /fullname:"Jean Pascal" /passwordchg:Yes
+pause
+net user Sylvain q1w2e3 /add /active:No /comment:"Compte Utilisateur" /expires:Never /fullname:"Sylvain Cossette" /passwordchg:No
+net user Andree 567890 /add /active:Yes /comment:"invite" /expires:2017-04-21 /fullname:"Andree Watters" /passwordchg:No
+net user Yuri w2e3r4 /add /active:Yes /comment:"Compte stagiaire" /expires:2017-05-01 /fullname:"Yuri Gagarine" /passwordchg:No
+net user Pierre Allos /add /active:No /comment:"Compte Directeur" /expires:Never /fullname:"Pierre Lambert" /passwordchg:Yes
+net user Samuel plaine /add /active:Yes /comment:"Compte Consultant" /expires:2017-06-01 /fullname:"Samuel de Champlain" /passwordchg:No
+pause
