40d0718c53e0c3461aa9030bbedd1f2d29c38432
Oleksandr Dziuba <alex.dzuba@gmail.com>
2024-04-27 19:08:11 -0400

    new file:   Windows/Information personnelles.bat

---
+++ Windows/Information personnelles.bat
@@ -0,0 +1,13 @@
+@echo off
+title Information personneles
+color f5
+echo je vais vous poser quelques questions
+pause
+set /p prenom=Quel est votrte prenom?:
+set /p nom=Quel est votre nom?:
+set /p age=Quel est votre age?:
+pause
+
+echo je sais que vous avez %age% et que vous appelez %prenom% %age%
+pause
+exit
